df4496063169514f2fc8085370d1f46d130982e7
PinaPhD <amwangi@alumni.cmu.edu>
2025-02-10 15:02:51 +0100

    update network topology with SNORT IDS nodes

--- DataPlane/topology.py
+++ DataPlane/topology.py
@@ -39,6 +39,35 @@ class sdnnet(Topo):
         v4 = self.addHost('v4')
         v5 = self.addHost('v5')
         
+        #Creating the SNORT IDS Nodes that are strategically placed in the WTGs, ECP and vPAC server clusters, and for inter-switch between spine and leaf switches
+        d1 = self.addHost('d1')
+        d2 = self.addHost('d2')
+        d3 = self.addHost('d3')
+        d4 = self.addHost('d4')
+        d5 = self.addHost('d5')
+        d6 = self.addHost('d6')
+        d7 = self.addHost('d7')
+        d8 = self.addHost('d8')
+        d9 = self.addHost('d9')
+        d10 = self.addHost('d10')
+        d11 = self.addHost('d11')
+        d12 = self.addHost('d12')
+        d13 = self.addHost('d13')
+        d14 = self.addHost('d14')
+        d15 = self.addHost('d15')
+        d16 = self.addHost('d16')
+        d17 = self.addHost('d17')
+        d18 = self.addHost('d18')
+        d19 = self.addHost('d19')
+        d20 = self.addHost('d20')
+        d21 = self.addHost('d21')
+        d22 = self.addHost('d22')
+        d23 = self.addHost('d23')
+        d24 = self.addHost('d24')
+        d25 = self.addHost('d25')
+        d26 = self.addHost('d26')
+        
+        
         # Creating LDAQ and Actuators for the WTGs
         r1 = self.addHost('r1')
         r2 = self.addHost('r2')
@@ -123,21 +152,25 @@ class sdnnet(Topo):
         self.addLink(s1, s6)
         self.addLink(s1, s7)
         self.addLink(s1, s8)
+        self.addLink(s1, d26)
 
         self.addLink(s2, s5)
         self.addLink(s2, s6)
         self.addLink(s2, s7)
         self.addLink(s2, s8)
+        self.addLink(s2, d25)
 
         self.addLink(s3, s5)
         self.addLink(s3, s6)
         self.addLink(s3, s7)
         self.addLink(s3, s8)
+        self.addLink(s3, d24)
 
         self.addLink(s4, s5)
         self.addLink(s4, s6)
         self.addLink(s4, s7)
         self.addLink(s4, s8)
+        self.addLink(s4, d23)
 
         # Connecting the Distribution and Access Layers
         self.addLink(s5, s9)
@@ -172,12 +205,14 @@ class sdnnet(Topo):
         self.addLink(e1,s19)
         self.addLink(e2,s20)
         self.addLink(e3,s10)
+        self.addLink(d21,s10)
         self.addLink(e4,s19)
         self.addLink(e5,s13)
         
         self.addLink(v1,s12)
         self.addLink(v2,s11)
         self.addLink(v3,s15)
+        self.addLink(d22,s15)
         self.addLink(v4,s17)
         self.addLink(v5,s19)
 
@@ -235,64 +270,83 @@ class sdnnet(Topo):
         #Linking local data acquisition modules and actuators to each wtg switches
         self.addLink(r1, s21)
         self.addLink(b1, s21)
+        self.addLink(d1, s21)
 
         self.addLink(r2, s22)
         self.addLink(b2, s22)
+        self.addLink(d2, s22)
 
         self.addLink(r3, s23)
         self.addLink(b3, s23)
+        self.addLink(d3, s23)
 
         self.addLink(r4, s24)
         self.addLink(b4, s24)
+        self.addLink(d4, s24)
 
         self.addLink(r5, s25)
         self.addLink(b5, s25)
+        self.addLink(d5, s25)
 
         self.addLink(r6, s26)
         self.addLink(b6, s26)
+        self.addLink(d6, s26)
 
         self.addLink(r7, s27)
         self.addLink(b7, s27)
+        self.addLink(d7, s27)
 
         self.addLink(r8, s28)
         self.addLink(b8, s28)
+        self.addLink(d8, s28)
 
         self.addLink(r9, s29)
         self.addLink(b9, s29)
+        self.addLink(d9, s29)
 
         self.addLink(r10, s30)
         self.addLink(b10, s30)
+        self.addLink(d10, s30)
 
         self.addLink(r11, s31)
         self.addLink(b11, s31)
+        self.addLink(d11, s31)
 
         self.addLink(r12, s32)
         self.addLink(b12, s32)
+        self.addLink(d12, s32)
 
         self.addLink(r13, s33)
         self.addLink(b13, s33)
+        self.addLink(d13, s33)
 
         self.addLink(r14, s34)
         self.addLink(b14, s34)
+        self.addLink(d14, s34)
 
         self.addLink(r15, s35)
         self.addLink(b15, s35)
+        self.addLink(d15, s35)
 
         self.addLink(r16, s36)
         self.addLink(b16, s36)
+        self.addLink(d16, s36)
 
         self.addLink(r17, s37)
         self.addLink(b17, s37)
+        self.addLink(d17, s37)
 
         self.addLink(r18, s38)
         self.addLink(b18, s38)
+        self.addLink(d18, s38)
 
         self.addLink(r19, s39)
         self.addLink(b19, s39)
+        self.addLink(d19, s39)
 
         self.addLink(r20, s40)
         self.addLink(b20, s40)
-        
+        self.addLink(d20, s40)
 
     
 topos = {'mytopo': (lambda: sdnnet() )}
