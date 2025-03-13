b0d9309bafe001504c7ab30e0f60c62f3040246e
PinaPhD <amwangi@alumni.cmu.edu>
2025-03-13 10:16:20 +0100

    updated data files and notes

--- Data_Files_and_Notes/host_configuration.py
+++ Data_Files_and_Notes/host_configuration.py
@@ -5,24 +5,20 @@ Created on Fri Feb 21 13:35:58 2025
 
 @author: Agrippina Mwangi
 """
-<<<<<<< HEAD
 
 #Importing relevant helper functions
 import random
 import requests
 import time
-
-
-=======
-import random
+import numpy as np
 import pandas as pd
->>>>>>> b9230616b02a92a8272fdb682ea3e556df69265b
 from Observe import current_network_state   #Loads the current network state as observed real-time
 
 
+'''
+Preliminary I: Subnet Pools, K, for different regions
+'''
 
-
-#Preliminary I: Subnet Pools, K, for different regions
 subnet_pools = {
     "WTG1": {"subnet": "192.168.16.0/25", "rangeStart": "192.168.16.10", "rangeEnd": "192.168.16.126"},
     "WTG2": {"subnet": "192.168.16.128/25", "rangeStart": "192.168.16.139", "rangeEnd": "192.168.16.254"},
@@ -65,3 +61,23 @@ s2h_df = pd.DataFrame(switch_to_host_mapping)
 
 #Sorting the dataframe by switch id 
 s2h_df_sorted = s2h_df.sort_values(by="switch_id").reset_index(drop=True)
+
+#Checking the host annotations from the Mininet network links mapping
+host_names = ['d26', 'd25', 'd24', 'd23', 'm1', 'm2', 'd21', 'e3', 'm3', 'v2', 'v1', 'm4', 'm5',
+ 'e5', 'q1', 'v3', 'q2', 'd22', 'q3', 'q4', 'v4', 'q5', 'e4', 'v5', 'e1', 'e2', 'b1',
+ 'r1', 'd1', 'r2', 'd2', 'b2', 'b3', 'r3', 'd3', 'r4', 'b4', 'd4', 'b5', 'd5', 'r5',
+ 'r6', 'b6', 'd6', 'r7', 'd7', 'b7', 'b8', 'r8', 'd8', 'd9', 'r9', 'b9', 'd10', 'r10',
+ 'b10', 'd11', 'r11', 'b11', 'r12', 'd12', 'b12', 'd13', 'b13', 'r13', 'r14', 'd14',
+ 'b14', 'b15', 'd15', 'r15', 'd16', 'b16', 'r16', 'r17', 'd17', 'b17', 'r18', 'd18',
+ 'b18', 'r19', 'b19', 'd19', 'b20', 'r20', 'd20']
+
+
+#Appending them to the switch/port to host mapping on column host_names
+s2h_df_sorted['host_names']=host_names
+
+
+'''
+Preliminary II: Assign each host an IP from the tuple:
+HIP = (rIP, vIP1, vIP2)
+'''
+
